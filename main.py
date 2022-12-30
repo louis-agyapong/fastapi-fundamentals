a82db529ec7cbdd549c43353cd65bc4dcffe56d4
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2022-12-30 22:31:33 +0000

    Predefined values with Enum

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from enum import Enum
+from pyexpat import model
 
 from fastapi import FastAPI
+
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
 
+
 app = FastAPI()
+
 
 @app.get("/")
 async def root():
@@ -17,3 +21,17 @@ async def root():
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+
+@app.get("models/{model_name}")
+async def get_model(model_name: ModelName):
+    """
+    Predefined values using Enum
+    """
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
