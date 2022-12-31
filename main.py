c42c013f7df3cec835bd5a9c93104c689fdd91cb
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2022-12-31 13:26:24 +0000

    Request body + path parameters

--- main.py
+++ main.py
@@ -44,6 +44,10 @@ async def create_item(item: Item):
         item_dict.update({"price_with_tax": price_with_tax})
     return item_dict
 
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
+
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
