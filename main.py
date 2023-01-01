5294e354a3c6d819e0616e46644a3b70bb723621
Louis Agyapong <47106199+louis-agyapong@users.noreply.github.com>
2023-01-01 16:13:50 +0000

    Validations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 
 
@@ -27,10 +27,12 @@ async def root():
     return {"message": "Hello world"}
 
 
-@app.get("/items/{item_id}")
-async def read_user_item(item_id: str, needy: str, skip: int = 0, limit: int | None = None):
-    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
-    return item
+@app.get("/items/")
+async def read_items(q: str | None = Query(default=None, max_length=50)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
 
 
 @app.post("/items/")
@@ -38,14 +40,18 @@ async def create_item(item: Item):
     """
     Create item
     """
-    item_dict = item.dict()
+    item_dict: dict = item.dict()
     if item.tax:
         price_with_tax: float = item.price + item.tax
         item_dict.update({"price_with_tax": price_with_tax})
     return item_dict
 
+
 @app.put("/items/{item_id}")
-async def create_item(item_id: int, item: Item):
+async def create_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        return result.update({"q": q})
     return {"item_id": item_id, **item.dict()}
 
 
@@ -61,6 +67,7 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
 
 
 @app.get("/path/{file_path:path}")
